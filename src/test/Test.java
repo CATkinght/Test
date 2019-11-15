4fec218007068516a22024262fb0c33f8807e52c
Administrator <Administrator@OS-201907151534>
2019-11-15 20:40:34 +0800

    11.15-20.40

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,13 +1,13 @@
 package test;
 
 public class Test {
+	private int id;
+	private static String name;
+	
+	public void set(String name){
+		this.name = name;
+	}
 	public static void main(String[] args) {
-		private int id;
-		private String name;
-		
-		public vodi set(String name){
-			this.name = name;
-		}
 		System.out.println(name);
 		System.out.println("Hello gitHub");
 		System.err.println("adfas ");
