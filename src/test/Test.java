132d85e9d5f82e60ad3da236f17539e1648c7f38
zlk <54013235+CATkinght@users.noreply.github.com>
2019-11-15 20:38:18 +0800

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,6 +2,13 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
+		private int id;
+		private String name;
+		
+		public vodi set(String name){
+			this.name = name;
+		}
+		System.out.println(name);
 		System.out.println("Hello gitHub");
 		System.err.println("adfas ");
 	}
